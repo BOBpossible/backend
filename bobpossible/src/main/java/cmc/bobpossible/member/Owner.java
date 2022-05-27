08bc42ab5e126ff8bd821715f9a922a0f6352631
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-05-28 04:27:50 +0900

    initial init

---
+++ bobpossible/src/main/java/cmc/bobpossible/member/Owner.java
@@ -0,0 +1,9 @@
+package cmc.bobpossible.member;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Owner extends Member{
+
+
+}
---
+++ bobpossible/src/main/java/cmc/bobpossible/store_image/Interior.java
@@ -0,0 +1,7 @@
+package cmc.bobpossible.store_image;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Interior extends StoreImage{
+}
