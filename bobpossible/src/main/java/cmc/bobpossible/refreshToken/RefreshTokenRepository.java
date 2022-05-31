65f860b36de6efa29949c3970e824e70fc7f57a3
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-06-01 02:39:38 +0900

    feat:[LJBV-30] refresh token 갱신 API 추가

---
+++ bobpossible/src/main/java/cmc/bobpossible/refreshToken/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package cmc.bobpossible.refreshToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByKey(String key);
+}
