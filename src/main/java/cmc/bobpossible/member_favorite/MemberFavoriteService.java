b76153bdb6fe7e05b9ab55001190b476006ec5db
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-06-17 20:14:11 +0900

    refactor: favorite -> category 이름 변경

--- src/main/java/cmc/bobpossible/member_favorite/MemberFavoriteService.java
+++ src/main/java/cmc/bobpossible/member_favorite/MemberFavoriteService.java
@@ -2,8 +2,8 @@ package cmc.bobpossible.member_favorite;
 
 import cmc.bobpossible.config.BaseException;
 import cmc.bobpossible.config.auth.SecurityUtil;
-import cmc.bobpossible.favorite.Favorite;
-import cmc.bobpossible.favorite.FavoriteRepository;
+import cmc.bobpossible.favorite.Category;
+import cmc.bobpossible.favorite.CategoryRepository;
 import cmc.bobpossible.member.Member;
 import cmc.bobpossible.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +18,7 @@ import static cmc.bobpossible.config.BaseResponseStatus.*;
 public class MemberFavoriteService {
 
     private final MemberRepository memberRepository;
-    private final FavoriteRepository favoriteRepository;
+    private final CategoryRepository categoryRepository;
     private final MemberFavoriteRepository memberFavoriteRepository;
 
     @Transactional
@@ -29,10 +29,10 @@ public class MemberFavoriteService {
                 .orElseThrow(() -> new BaseException(CHECK_QUIT_USER));
 
         // 선호 음식 조회
-        Favorite favorite = favoriteRepository.findById(favoriteId)
+        Category category = categoryRepository.findById(favoriteId)
                 .orElseThrow(() -> new BaseException(FAVORITES_ID_NOT_EXIST));
 
-        MemberFavorite memberFavorite = MemberFavorite.create(member, favorite);
+        MemberFavorite memberFavorite = MemberFavorite.create(member, category);
 
         memberFavoriteRepository.save(memberFavorite);
     }
