c252fb42eb280385ddc1a8553452749c6c5ba217
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-06-23 05:24:37 +0900

    feat: mission validation check

--- src/main/java/cmc/bobpossible/mission/MissionService.java
+++ src/main/java/cmc/bobpossible/mission/MissionService.java
@@ -4,6 +4,7 @@ import cmc.bobpossible.config.BaseException;
 import cmc.bobpossible.config.auth.SecurityUtil;
 import cmc.bobpossible.member.MemberRepository;
 import cmc.bobpossible.member.entity.Member;
+import cmc.bobpossible.utils.DistanceCalculator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +26,25 @@ public class MissionService {
         Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId())
                 .orElseThrow(() -> new BaseException(CHECK_QUIT_USER));
 
-        return missionRepository.findByMember(member);
+        List<Mission> missions = missionRepository.findByMember(member);
+
+
+        // 현재 미션들의 만료일 체크
+        missions.forEach(Mission::checkValidation);
+
+        // 현재 미션 할당된 미션 없음
+        if(missions.isEmpty()){
+            // 미션할당
+                // 같은 동 가게 목록
+                    // 거리 계산
+                        //DistanceCalculator.distance()
+                    // 선호도 계산
+                        // category check
+                    // 저번주 확인
+                    // 저저번주 확인
+        }
+
+        return missions;
     }
 
     public List<Mission> getMissionOnProgress() throws BaseException {
