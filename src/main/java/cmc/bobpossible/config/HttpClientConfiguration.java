3535ec85c18b306a33e6f3c04645fab8cdd636af
JinbeomKim <kst20026@gmail.com>
2022-10-22 19:37:19 +0900

    fix: fcm 느려짐 문제 개선

---
+++ src/main/java/cmc/bobpossible/config/HttpClientConfiguration.java
@@ -0,0 +1,14 @@
+package cmc.bobpossible.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientConfiguration {
+
+    @Bean
+    public OkHttpClient httpClient() {
+        return new OkHttpClient();
+    }
+}
